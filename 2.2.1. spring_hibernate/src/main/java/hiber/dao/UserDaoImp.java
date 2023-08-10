ddf01fa6b2b207e6787d941e2b44c1728753eb9f
Maksim Bardikov <maximelanosx@yandex.ru>
2023-08-10 20:43:03 +0300

    task completed

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,5 +1,6 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,4 +27,33 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+   @Override
+   public void add(Car car) {
+      sessionFactory.getCurrentSession().save(car);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<Car> listCars() {
+      TypedQuery<Car> query = sessionFactory.getCurrentSession().createQuery("from Car");
+      return query.getResultList();
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public User getOwner(String model, int series) {
+      TypedQuery<Car> carQuery = sessionFactory.getCurrentSession().createQuery("from Car where model = :model and series = :series")
+              .setParameter("model", model)
+              .setParameter("series", series);
+      List<Car> listCars = carQuery.getResultList();
+      if (!listCars.isEmpty()) {
+         Car car = listCars.get(0);
+         List<User> listUser = listUsers();
+         return listUser.stream()
+                 .filter(user -> user.getCar().equals(car))
+                 .findAny()
+                 .orElse(null);
+      }
+      return null;
+   }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -1,10 +1,21 @@
 package hiber.service;
 
+import hiber.model.Car;
 import hiber.model.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface UserService {
     void add(User user);
     List<User> listUsers();
+
+    @Transactional
+    void add(Car car);
+
+    @Transactional(readOnly = true)
+    List<Car> listCars();
+
+    @Transactional
+    User getOwner(String model, int series);
 }
